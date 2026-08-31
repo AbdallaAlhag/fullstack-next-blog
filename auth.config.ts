3f28354e879f9b4353acaa7ffd3e74366c9d785d
Abdalla Alhag <61675114+AbdallaAlhag@users.noreply.github.com>
2026-08-31 10:46:55 -0700

    create proxy, auth.config, and our auth file. Auth file should compare passwords and get user that works with our login form. Need to update types and make a user type and update our db as in create a user tabel that has a primary key withour post table

--- auth.config.ts
+++ auth.config.ts
@@ -4,5 +4,19 @@ export const authConfig = {
   pages: {
     signIn: "/login",
   },
-  providers: [],
+  // what this does is only allow logged in users on certain pages but my current setup has no retrisited pages atm.
+  // callbacks: {
+  //   authorized({ auth, request: { nextUrl } }) {
+  //     const isLoggedIn = !!auth?.user;
+  //     const isOnDashboard = nextUrl.pathname.startsWith("/");
+  //     if (isOnDashboard) {
+  //       if (isLoggedIn) return true;
+  //       return false; // Redirect unauthenticated users to login page
+  //     } else if (isLoggedIn) {
+  //       return Response.redirect(new URL("/", nextUrl));
+  //     }
+  //     return true;
+  //   },
+  // },
+  providers: [], // Add providers with an empty array for now
 } satisfies NextAuthConfig;
